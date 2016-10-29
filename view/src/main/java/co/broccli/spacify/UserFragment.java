14045aef31ae2ca4273b53c6a835b17048e02484
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-10-29 13:40:17 +0200

    Revert "Revert "test""
    This reverts commit 47fbf60fa0dd0d5fa6ac7cacf3c778d4a683841e.

--- view/src/main/java/co/broccli/spacify/UserFragment.java
+++ view/src/main/java/co/broccli/spacify/UserFragment.java
@@ -2,6 +2,7 @@ package co.broccli.spacify;
 
 
 import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
+import com.facebook.drawee.view.SimpleDraweeView;
 
 import co.broccli.logic.SessionManager;
 import co.broccli.logic.model.protectedPath.protectedPath;
@@ -45,42 +48,46 @@ public class UserFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_user, container, false);
 
-        progressBar = (ContentLoadingProgressBar) view.findViewById(R.id.content_loading);
-        textMessage = (TextView) view.findViewById(R.id.textMessage);
-        logoutFAB = (FloatingActionButton)  view.findViewById(R.id.logoutFAB);
+        Uri uri = Uri.parse("https://gitlab.com/uploads/user/avatar/56386/tt_avatar_small.jpg");
+        SimpleDraweeView draweeView = (SimpleDraweeView) view.findViewById(R.id.user_pic_view);
+        draweeView.setImageURI(uri);
 
-        logoutFAB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sessionManager = new SessionManager(getContext());
+//        progressBar = (ContentLoadingProgressBar) view.findViewById(R.id.content_loading);
+//        textMessage = (TextView) view.findViewById(R.id.textMessage);
+//        logoutFAB = (FloatingActionButton)  view.findViewById(R.id.logoutFAB);
 
-                final ProgressDialog progressDialog = new ProgressDialog(getContext(),
-                        R.style.AppTheme_Dark_Dialog);
-                progressDialog.setIndeterminate(true);
-                progressDialog.setMessage("Signing out...");
-                progressDialog.show();
-
-                // Logout user from the server (Revoke Access Token)
-                ApiInterface apiService =
-                        ApiClient.createService(ApiInterface.class, getContext());
-                Call<ResponseBody> call = apiService.logout();
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        // Do nothing
-                    }
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        // Do nothing
-                    }
-                });
-
-                progressDialog.dismiss();
-
-                // Logout user from app
-                sessionManager.logoutUser(LoginActivity.class);
-            }
-        });
+//        logoutFAB.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sessionManager = new SessionManager(getContext());
+//
+//                final ProgressDialog progressDialog = new ProgressDialog(getContext(),
+//                        R.style.AppTheme_Dark_Dialog);
+//                progressDialog.setIndeterminate(true);
+//                progressDialog.setMessage("Signing out...");
+//                progressDialog.show();
+//
+//                // Logout user from the server (Revoke Access Token)
+//                ApiInterface apiService =
+//                        ApiClient.createService(ApiInterface.class, getContext());
+//                Call<ResponseBody> call = apiService.logout();
+//                call.enqueue(new Callback<ResponseBody>() {
+//                    @Override
+//                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//                        // Do nothing
+//                    }
+//                    @Override
+//                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                        // Do nothing
+//                    }
+//                });
+//
+//                progressDialog.dismiss();
+//
+//                // Logout user from app
+//                sessionManager.logoutUser(LoginActivity.class);
+//            }
+//        });
 
         // Inflate the layout for this fragment
         return view;
@@ -90,22 +97,22 @@ public class UserFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ApiInterface apiService =
-                ApiClient.createService(ApiInterface.class, getContext());
-
-        Call<protectedPath> call = apiService.getUser();
-        call.enqueue(new Callback<protectedPath>() {
-            @Override
-            public void onResponse(Call<protectedPath> call, Response<protectedPath> response) {
-
-                progressBar.setVisibility(View.GONE);
-                textMessage.setText(response.body().getUser().getName());
-            }
-
-            @Override
-            public void onFailure(Call<protectedPath> call, Throwable t) {
-                progressBar.setVisibility(View.GONE);
-            }
-        });
+//        ApiInterface apiService =
+//                ApiClient.createService(ApiInterface.class, getContext());
+//
+//        Call<protectedPath> call = apiService.getUser();
+//        call.enqueue(new Callback<protectedPath>() {
+//            @Override
+//            public void onResponse(Call<protectedPath> call, Response<protectedPath> response) {
+//
+//                progressBar.setVisibility(View.GONE);
+//                textMessage.setText(response.body().getUser().getName());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<protectedPath> call, Throwable t) {
+//                progressBar.setVisibility(View.GONE);
+//            }
+//        });
     }
 }
