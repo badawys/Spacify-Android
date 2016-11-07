a139e289b715a349cd79e927310290c20dc529dc
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-11-07 14:09:26 +0200

    Restucture Signup functions and add comments

--- logic/src/main/java/co/broccli/logic/SpacifyApi.java
+++ logic/src/main/java/co/broccli/logic/SpacifyApi.java
@@ -5,11 +5,32 @@ import android.util.Log;
 
 public class SpacifyApi {
 
+    /**
+     * The SpacifyApi class instance
+     */
     private static SpacifyApi mInstance  = null;
+
+    /**
+     * The Session Manager instance
+     */
     private  SessionManager sessionManager;
+
+    /**
+     *  SpacifyApi class name (to be used as Tag in logging)
+     */
     private static final String TAG = SpacifyApi.class.getSimpleName();
+
+    /**
+     * The reference of the login activity class
+     */
     private static Class<?> mloginActivity;
 
+    /**
+     *  Initialize new instance of SpacifyApi class
+     *
+     * @param context the context of current state of the application
+     * @return the singleton instance of the SpacifyApi class
+     */
     public static SpacifyApi initialize (Context context) {
         //Check if SpacifyApi as already been initialized
         if(mInstance == null) {
@@ -25,14 +46,29 @@ public class SpacifyApi {
         return mInstance;
     }
 
+    /**
+     * Initialize new instance of SessionManager class
+     *
+     * @param context the context of current state of the application
+     */
     private void initializeSessionManager (Context context) {
         sessionManager = new SessionManager(context);
     }
 
+    /**
+     *  Set the Login Activity reference
+     *
+     * @param loginActivity set reference of the login activity class
+     */
     public void setLoginActivity (Class<?> loginActivity) {
         mloginActivity = loginActivity;
     }
 
+    /**
+     * All the authentication and authorization operations
+     *
+     * @return instance if Auth class
+     */
     public static Auth auth () {
         return Auth.getInstance(mInstance.sessionManager, mloginActivity);
     }
