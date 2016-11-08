063b98e6e2dc6e9b4b628e8ee99184cb12a0a7b5
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-11-08 14:51:31 +0200

    LeakCanary added (Memory Leak Debug Tool)

--- view/src/main/java/co/broccli/spacify/SpacifyApp.java
+++ view/src/main/java/co/broccli/spacify/SpacifyApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.squareup.leakcanary.LeakCanary;
 
 import co.broccli.logic.SpacifyApi;
 
@@ -13,6 +14,14 @@ public class SpacifyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
         final Thread.UncaughtExceptionHandler oldHandler =
                 Thread.getDefaultUncaughtExceptionHandler();
 
