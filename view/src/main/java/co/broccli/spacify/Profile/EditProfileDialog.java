beddf0a5e1eff3d64ab0be8ebed24447a1b325e5
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-11-09 18:18:00 +0200

    Loading added to edit profile dialog

--- view/src/main/java/co/broccli/spacify/Profile/EditProfileDialog.java
+++ view/src/main/java/co/broccli/spacify/Profile/EditProfileDialog.java
@@ -7,23 +7,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.victor.loading.rotate.RotateLoading;
+
 import co.broccli.logic.Callback;
 import co.broccli.logic.SpacifyApi;
 import co.broccli.logic.model.profile.User;
 import co.broccli.spacify.R;
 import fr.tvbarthel.lib.blurdialogfragment.SupportBlurDialogFragment;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 
 public class EditProfileDialog extends SupportBlurDialogFragment {
 
+    private LinearLayout dialogContent;
+    private RotateLoading rotateLoading;
     private SimpleDraweeView profilePhoto;
     private EditText userName;
     private EditText email;
+    private FancyButton cancelButton;
+    private FancyButton okButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,9 +41,16 @@ public class EditProfileDialog extends SupportBlurDialogFragment {
 
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 
+        dialogContent = (LinearLayout) v.findViewById(R.id.dialog_content);
+        rotateLoading = (RotateLoading) v.findViewById(R.id.rotateloading);
         profilePhoto = (SimpleDraweeView) v.findViewById(R.id.user_pic_view);
         userName = (EditText) v.findViewById(R.id.input_name);
         email = (EditText) v.findViewById(R.id.input_email);
+        cancelButton = (FancyButton) v.findViewById(R.id.cancelButton);
+        okButton = (FancyButton) v.findViewById(R.id.okButton);
+
+        dialogContent.setVisibility(View.INVISIBLE);
+        rotateLoading.start();
 
         SpacifyApi.profile().getUserProfile(getContext(), new Callback<User>() {
             @Override
@@ -42,10 +58,26 @@ public class EditProfileDialog extends SupportBlurDialogFragment {
                 setProfilePhoto (user.getPhoto());
                 userName.setText(user.getName());
                 email.setText(user.getEmail());
+                rotateLoading.stop();
+                dialogContent.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onError(String errorMessage) {
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDialog().dismiss();
+            }
+        });
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
