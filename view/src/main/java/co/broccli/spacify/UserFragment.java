bdb16ca7309ff102aeba33444ba1aa6a2d8bcb36
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-11-12 15:43:54 +0200

    Fix blured profile header

--- view/src/main/java/co/broccli/spacify/UserFragment.java
+++ view/src/main/java/co/broccli/spacify/UserFragment.java
@@ -1,6 +1,7 @@
 package co.broccli.spacify;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,15 +9,25 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.facebook.cache.common.CacheKey;
+import com.facebook.cache.common.SimpleCacheKey;
+import com.facebook.common.references.CloseableReference;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory;
+import com.facebook.imagepipeline.request.BasePostprocessor;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import co.broccli.logic.Callback;
@@ -25,6 +36,7 @@ import co.broccli.logic.model.profile.User;
 import co.broccli.spacify.Profile.EditProfileDialog;
 import co.broccli.spacify.Profile.JoinedSpaces.JoinedSpacesFragmant;
 import co.broccli.spacify.Profile.MySpaces.MySpacesFragment;
+import co.broccli.spacify.Settings.SettingsActivity;
 import fr.tvbarthel.lib.blurdialogfragment.SupportBlurDialogFragment;
 import mehdi.sakout.fancybuttons.FancyButton;
 
@@ -103,6 +115,9 @@ public class UserFragment extends Fragment {
             getProfileData();
     }
 
+    /**
+     * ToDo: Add Description
+     */
     private void getProfileData () {
         SpacifyApi.profile().getUserProfile(getContext(), new Callback<User>() {
             @Override
@@ -124,32 +139,70 @@ public class UserFragment extends Fragment {
     private void setProfilePhoto (String url) {
 
         Uri uri = Uri.parse("http://spacify.s3.amazonaws.com/" + url);
-//        FrascoRepeatedPostProcessor repeatedPostProcessor = new FrascoRepeatedPostProcessor();
+
+        Postprocessor blurPostprocessor = new BasePostprocessor() {
+
+            private static final int BITMAP_SCALE_DOWN = 4;
+            private static final int BLUR_RADIUS = 25;
+
+            @Override
+            public String getName() {
+                return "blurPostprocessor";
+            }
+
+            public CacheKey getPostprocessorCacheKey() {
+                return new SimpleCacheKey("profile_header_background");
+            }
+
+            @Override
+            public CloseableReference<Bitmap> process(Bitmap sourceBitmap, PlatformBitmapFactory bitmapFactory) {
+
+                int width = Math.round(sourceBitmap.getWidth() / BITMAP_SCALE_DOWN);
+                int height = Math.round(sourceBitmap.getHeight() / BITMAP_SCALE_DOWN);
+
+                CloseableReference<Bitmap> sourceBitmapRef = bitmapFactory.createScaledBitmap(sourceBitmap, width, height, false);
+                CloseableReference<Bitmap> distBitmapRef = bitmapFactory.createBitmap(sourceBitmapRef.get());
+                try {
+                    Bitmap inputBitmap  = sourceBitmapRef.get();
+                    Bitmap outputBitmap = distBitmapRef.get();
+
+                    RenderScript rs = RenderScript.create(getContext());
+                    ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+                    Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+                    Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+                    theIntrinsic.setRadius(BLUR_RADIUS);
+                    theIntrinsic.setInput(tmpIn);
+                    theIntrinsic.forEach(tmpOut);
+                    tmpOut.copyTo(outputBitmap);
+
+                    return CloseableReference.cloneOrNull(distBitmapRef);
+                } finally {
+                    CloseableReference.closeSafely(distBitmapRef);
+                }
+            }
+        };
 
         ImageRequest profilePhotoRequest = ImageRequestBuilder
                 .newBuilderWithSource(uri)
                 .build();
-//        ImageRequest backgroundPhotoRequest = ImageRequestBuilder
-//                .newBuilderWithSource(uri)
-//                .setPostprocessor(repeatedPostProcessor)
-//                .build();
+        ImageRequest backgroundPhotoRequest = ImageRequestBuilder
+                .newBuilderWithSource(uri)
+                .setPostprocessor(blurPostprocessor)
+                .build();
 
         PipelineDraweeController photoController =
                 (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
                         .setImageRequest(profilePhotoRequest)
                         .setOldController(profilePhoto.getController())
                         .build();
-//        PipelineDraweeController backgroundController =
-//                (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
-//                        .setImageRequest(backgroundPhotoRequest)
-//                        .setOldController(headerBackground.getController())
-//                        .build();
+        PipelineDraweeController backgroundController =
+                (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+                        .setImageRequest(backgroundPhotoRequest)
+                        .setOldController(headerBackground.getController())
+                        .build();
 
         profilePhoto.setController(photoController);
-//        headerBackground.setController(backgroundController);
-
-//        repeatedPostProcessor.apply(new BlurPostprocessor(getContext(), 150));
-//        repeatedPostProcessor.apply(new ColorFilterPostprocessor(Color.argb(80, 0, 0, 0)));
+        headerBackground.setController(backgroundController);
     }
 
     /**
@@ -214,6 +267,8 @@ public class UserFragment extends Fragment {
      * ToDo: Add Description
      */
     private void onClickSettings() {
+//        Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
+//        getActivity().startActivity(settingsIntent);
     }
 
 }
