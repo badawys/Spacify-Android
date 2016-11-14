58dde269b516245c4cdd959c5bc59f24886a75c2
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-11-14 06:01:58 +0200

    Location map added

--- view/src/main/java/co/broccli/spacify/NearbyFragment.java
+++ view/src/main/java/co/broccli/spacify/NearbyFragment.java
@@ -1,18 +1,43 @@
 package co.broccli.spacify;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.victor.loading.rotate.RotateLoading;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class NearbyFragment extends Fragment {
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesProvider;
 
+
+public class NearbyFragment extends Fragment implements  OnMapReadyCallback, OnLocationUpdatedListener{
+
+    private LocationGooglePlayServicesProvider provider;
+    private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_ID = 1001;
+    private Circle mapCircle;
+    private CircleOptions circleOptions;
+    private RotateLoading rotateLoading;
 
     public NearbyFragment() {
         // Required empty public constructor
@@ -27,7 +52,80 @@ public class NearbyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nearby, container, false);
+        View view = inflater.inflate(R.layout.fragment_nearby, container, false);
+
+        rotateLoading = (RotateLoading) view.findViewById(R.id.rotateloading);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return  view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Location permission not granted
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_ID);
+            return;
+        }
+        startLocation();
+    }
+
+    private void startLocation() {
+        provider = new LocationGooglePlayServicesProvider();
+        provider.setCheckLocationSettings(true);
+
+        SmartLocation smartLocation = new SmartLocation.Builder(getContext()).logging(true).build();
+        smartLocation.location(provider).start(this);
+
+        circleOptions = new CircleOptions()
+                .radius(500)
+                .fillColor(0x551976D2)
+                .strokeColor(0x551976D2)
+                .strokeWidth(0);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_ID && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startLocation();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+            }
+        });
+        rotateLoading.start();
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                rotateLoading.stop();
+            }
+        });
+
+        Location lastLocation = SmartLocation.with(getContext()).location().getLastLocation();
+        if (lastLocation != null) {
+            onLocationUpdated(lastLocation);
+        }
+    }
+
+    @Override
+    public void onLocationUpdated(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14.5f);
+        if (mapCircle != null)
+            mapCircle.remove();
+        mMap.animateCamera(cameraUpdate);
+        mapCircle = mMap.addCircle(circleOptions.center(latLng));
+    }
 }
